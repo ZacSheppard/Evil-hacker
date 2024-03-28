63cf2fe30b46f6abcf558f4d3b029ecdca0dbdd3
Zac <ZSheppard@student.saintaug.nsw.edu.au>
2024-03-28 13:52:09 +1100

    made massive changes to the grid system as it wasnt working in the way i would like, also changed up the guess thing

--- evil conncetions.py
+++ evil conncetions.py
@@ -1,55 +1,71 @@
-import random #lets me use random
+import random
 
-#mr fongs twitter is @pitawater
-
-grid = [                  #creates 4x4 grid.
-    ["word","word","word","word"],
-    ["word","word","word","word"],
-    ["word","word","word","word"],
-    ["word","word","word","word"],
+grid = [                  # creates 4x4 grid.
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
 ]
 
-connections = [  #makes library's of words called connections, each connection has 4 words and a linking word/phrase
-    
-    {"linking_word" : "evil words/things",               #the phrase that links all the 4 words          
-    "words" : ["devious", "evil", "Zac", "naughty"]},  # the words included in the list
-   
-    {"linking_word" : "places you can find spiders",
-    "words" : ["haunted house", "web", "backyard", "haloween"]},
-    
-    {"linking_word" : "common things you see while on too much benadryl",
-    "words" : ["spiders", "shadow people", "the hat man", "demons "]},
-    
-    {"linking_word" : "ben and jerrys flavors ",
-    "words" : ["gimme smore", "vanilla", "coffee coffee buzz buzz buzz", "chunky monkey"]},
-   
-    {"linking_word" : "medieval helmets",
-    "words" : ["frogmouth", "kettle hat", "hounskull", "sallet"]},
-    
+connections = [  # makes library's of words called connections, each connection has 4 words and a linking word/phrase
+
+    {"linking_word": "evil words/things",  # the phrase that links all the 4 words
+     "words": ["devious", "evil", "Zac", "naughty"]},  # the words included in the list
+
+    {"linking_word": "places you can find spiders",
+     "words": ["haunted-house", "web", "backyard", "haloween"]},
+
+    {"linking_word": "common things you see while on too much benadryl",
+     "words": ["spiders", "shadow people", "the hat man", "demons "]},
+
+    {"linking_word": "ben and jerrys flavors ",
+     "words": ["gimme-smore", "vanilla", "coffee-coffee-buzz-buzz-buzz", "chunky-monkey"]},
+
+    {"linking_word": "medieval helmets",
+     "words": ["frogmouth", "kettle-hat", "hounskull", "sallet"]},
+
 ]
 
-selected_connections = random.sample(connections, 4)
+def fill_grid():
+    # Flatten the list of shuffled words
+    shuffled_words = []  #creates list of shuffled words
+    for connection in connections: #for loop to gather the single connection inside the full list of connections
+        for word in connection["words"]: #refers to each word inside of the inside of the connection
+            shuffled_words.append(word) #adds each word to the shuffled words list
 
- 
-for connection in selected_connections:
-    row = 0 
-    col = 0  # Reset column for each connection
-    for word in connection["words"]:
-        grid[row][col] = word
-        col += 1
-        if col == 4:  # If column reaches 4, move to the next row
-            row += 1
-            col = 0
+    # Shuffle the flattened list of words
+    random.shuffle(shuffled_words) #uses shuffle to shuffle the word inside the shuffled words list
 
-print(grid)
+    # Distribute the shuffled words across the grid   
+    for l in range(4):  #refers to the length of the list because its four things long
+        for w in range(4): #same thing but this time its
+            if shuffled_words:  #checks if there are still words in the shuffled words list 
+                grid[l][w] = shuffled_words.pop(0)  #adds word into spots inside of the grid using pop 
+
+    print_grid(grid)
+    return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(" | ".join(row))  # Add separator between words
+        print("-" * (len(row) * 10 + 3))  # Add horizontal line between rows
+
+fill_grid()
 
 #please can you type in your four words
 def player_guess():
-    guess = input("please guess your four words") 
-    while guess.isdigit():
-        guess = input("make sure your guess matches with the words in the grid")
-        print(grid)
-    return guess
+    guesses = []
+    guess_number = 1
+    for i in range(4):
+        guess = input("Guess #{}:     ".format(guess_number),)  #asks the player for they're guess, format adds integer into string
+        guess_number += 1  #adds a
+        guesses.append(guess)
+    print (guesses)
+    return guesses
+player_guess()
+    
+
+
 
 
 
--- evil test.py
+++ evil test.py
@@ -1,27 +1,53 @@
-def test_1():
-    category1 = {
-    "linking_word" : "evil words/things",
-    "words" : ["devious", "evil", "Zac", "naughty"]  
-    }
-    category2 = {
-    "linking_word" : "places you can find spiders",
-    "words" : ["haunted house", "web", "backyard", "haloween"]
-    }
-    category3 = {
-    "linking_word" : "common things you see while on too much benadryl",
-    "words" : ["spiders", "shadow people", "the hat man", "demons "]
-    }
-    category4 = {
-    "linking_word" : "ben and jerrys flavors ",
-    "words" : ["gimme smore", "vanilla", "coffee coffee buzz buzz buzz", "chunky monkey"]
-    }
-    category5 = {
-    "linking_word" : "medieval helmets",
-    "words" : ["frogmouth", "kettle hat", "hounskull", "sallet"]  
-    }
-    for word in category1['words']:
-        print(word)
+import random
 
-    print(category3["words"])
-    print(category3["linking_word"])
-    
+grid = [                  # creates 4x4 grid.
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
+    ["word", "word", "word", "word"],
+]
+
+connections = [  # makes library's of words called connections, each connection has 4 words and a linking word/phrase
+
+    {"linking_word": "evil words/things",  # the phrase that links all the 4 words
+     "words": ["devious", "evil", "Zac", "naughty"]},  # the words included in the list
+
+    {"linking_word": "places you can find spiders",
+     "words": ["haunted-house", "web", "backyard", "haloween"]},
+
+    {"linking_word": "common things you see while on too much benadryl",
+     "words": ["spiders", "shadow people", "the hat man", "demons "]},
+
+    {"linking_word": "ben and jerrys flavors ",
+     "words": ["gimme-smore", "vanilla", "coffee-coffee-buzz-buzz-buzz", "chunky-monkey"]},
+
+    {"linking_word": "medieval helmets",
+     "words": ["frogmouth", "kettle-hat", "hounskull", "sallet"]},
+
+]
+
+def fill_grid():
+    # Flatten the list of shuffled words
+    shuffled_words = []  #creates list of shuffled words
+    for connection in connections: #for loop to gather the single connection inside the full list of connections
+        for word in connection["words"]: #refers to each word inside of the inside of the connection
+            shuffled_words.append(word) #adds each word to the shuffled words list
+
+    # Shuffle the flattened list of words
+    random.shuffle(shuffled_words) #uses shuffle to shuffle the word inside the shuffled words list
+
+    # Distribute the shuffled words across the grid   
+    for l in range(4):  #refers to the length of the list because its four things long
+        for w in range(4): #same thing but this time its
+            if shuffled_words:  #checks if there are still words in the shuffled words list 
+                grid[l][w] = shuffled_words.pop(0)  #adds word into spots inside of the grid using the pop function 
+
+    print_grid(grid)
+    return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(" | ".join(row))  # Add separator between words
+        print("-" * (len(row) * 10 + 3))  # Add horizontal line between rows
+
+fill_grid()
