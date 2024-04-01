5f8c4f6d20f7756c7f9ef08b401fcffe6ee950e3
Zac <ZSheppard@student.saintaug.nsw.edu.au>
2024-04-01 14:41:48 +1100

    made a bunch of new functions, now i ust need to get the new grid working then ill have a working game

--- evil conncetions.py
+++ evil conncetions.py
@@ -1,5 +1,7 @@
 import random
 
+lives = 3
+
 grid = [                  # creates 4x4 grid.
     ["word", "word", "word", "word"],
     ["word", "word", "word", "word"],
@@ -26,22 +28,29 @@ connections = [  # makes library's of words called connections, each connection 
 
 ]
 
+selected_connections = random.sample(connections, 4)
+
+connection1, connection2, connection3, connection4 = selected_connections
+
+all_words = []
+
 def fill_grid():
+    global all_words
     # Flatten the list of shuffled words
     shuffled_words = []  #creates list of shuffled words
-    for connection in connections: #for loop to gather the single connection inside the full list of connections
+    for connection in selected_connections: #for loop to gather the single connection inside the full list of connections
         for word in connection["words"]: #refers to each word inside of the inside of the connection
             shuffled_words.append(word) #adds each word to the shuffled words list
-
     # Shuffle the flattened list of words
+    
     random.shuffle(shuffled_words) #uses shuffle to shuffle the word inside the shuffled words list
-
+    
     # Distribute the shuffled words across the grid   
     for l in range(4):  #refers to the length of the list because its four things long
         for w in range(4): #same thing but this time its width
             if shuffled_words:  #checks if there are still words in the shuffled words list 
                 grid[l][w] = shuffled_words.pop(0)  #adds word into spots inside of the grid ensuring each word is only used once
-
+                all_words.append(grid[l][w])
     print_grid(grid)
     return grid
 
@@ -52,20 +61,36 @@ def print_grid(grid):
 
 fill_grid()
 
+print(all_words)
+print(connection1)
 #please can you type in your four words
 def player_guess():
+    global guesses
     guesses = []
     guess_number = 1
-    for i in range(4):
-        guess = input("Guess #{}:     ".format(guess_number),)  #asks the player for they're guess, format adds integer into string
-        guess_number += 1  #adds a
-        guesses.append(guess)
+    while guess_number <= 4:  # Loop until 4 guesses are made
+        guess = input("Guess #{}: ".format(guess_number))  # Ask the player for their guess
+        if guess in all_words:
+            guesses.append(guess)
+            guess_number += 1  # Increment guess number only if the guess is correct
+        else:
+            print("Incorrect guess, Try again.")
     print (guesses)
     return guesses
 player_guess()
+
+
+def check_if_guess_correct():
+    global lives
+    for connection in selected_connections:
+        if set(guesses) == set(connection["words"]):
+            print(f"Correct! the connection was:  {connection['linking_word']}")
+            return
+    print("Incorrect")
+    lives -= 1
+
     
-
-
+check_if_guess_correct()
 
 
 
